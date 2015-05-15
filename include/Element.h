ceb8747481ecf014d98eba4145d25ec9cc08ca66
wpro428416 <wpro428416@gmail.com>
2015-05-15 12:25:10 +0800

    Create Element.h

---
+++ include/Element.h
@@ -0,0 +1,13 @@
+#ifndef ELEMENT_H_INCLUDED
+#define ELEMENT_H_INCLUDED
+class Element
+{
+public:
+    virtual void on();
+    virtual void off();
+private:
+protected:
+};
+
+
+#endif // ELEMENT_H_INCLUDED
