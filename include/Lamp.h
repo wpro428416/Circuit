97a452fd561edf22beb7e34725b9f56e298cd881
wpro428416 <wpro428416@gmail.com>
2015-05-15 12:24:19 +0800

    Create Lamp.h

---
+++ include/Lamp.h
@@ -0,0 +1,16 @@
+#ifndef LIGHT_H_INCLUDED
+#define LIGHT_H_INCLUDED
+
+#include "Element.h"
+
+class Lamp:public Element
+{
+public:
+    void on();
+    void off();
+private:
+protected:
+};
+
+
+#endif // LIGHT_H_INCLUDED
