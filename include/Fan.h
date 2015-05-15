fd3fe4639fb5acc58cfa7ef7f694036b062c35e1
wpro428416 <wpro428416@gmail.com>
2015-05-15 12:24:45 +0800

    Create Fan.h

---
+++ include/Fan.h
@@ -0,0 +1,17 @@
+#ifndef FAN_H_INCLUDED
+#define FAN_H_INCLUDED
+
+#include "Element.h"
+
+class Fan:public Element
+{
+public:
+    void on();
+    void off();
+private:
+protected:
+};
+
+
+
+#endif // FAN_H_INCLUDED
