552907ba4ff876a8f47489c3bccec5519ae9312f
wpro428416 <wpro428416@gmail.com>
2015-05-15 12:27:43 +0800

    Create Element.cpp

---
+++ src/Element.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include "Element.h"
+
+void Element::off(){
+    cout<<"this element is off"<<endl;
+}
+void Element::on(){
+    cout<<"this element is on"<<endl;
+}
